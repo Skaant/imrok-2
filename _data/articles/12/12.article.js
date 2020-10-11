ce8876bc474b8b357ba87c8b2a88245d6fc27b60
imrok le bleu <romaric.ruga@gmail.com>
2020-10-11 22:48:53 +0200

    article 12

---
+++ _data/articles/12/12.article.js
@@ -0,0 +1,115 @@
+export default {
+  id: '12',
+  title: "Réfléxions sur le financement de l'HIGHBS-BOK et les storyboards",
+  date: '11/10/2020',
+  tags: [ 'entrepreneuriat', 'highbs-bok.art', 'imrok.fr' ],
+  description: "Cet article se destine aux artistes et aux entrepreneurs, et aborde deux notions importantes : on devient ce que l'on fait, et quelle valeur donner à notre production.",
+  content: `Depuis que je suis re-passé *freelance* ([lien vers mon profil Malt](https://www.malt.fr/profile/romaricruga)),
+Le rapport temps / argent est devenu omniprésent.
+
+**La marge se creuse avec le temps gagné.**
+
+Mes projets ont commencé à porter la marque de cette pensée "rentable".
+
+Quelle valeur a le temps que je passe sur
+le framework [kami.js](https://github.com/Skaant/kami.js) ?
+Sur l'[HIGHBS-BOK](https://highbs-bok.art) ?
+Sur mes autres canaux et activités ?
+
+C'est une loi difficile de la vie,
+Que l'on a tendance à masquer derrière le cosmétique :
+
+**Pour vivre, il faut travailler.**
+
+Il faut aller chercher de l'énergie,
+En en dépensant le moins possible.
+
+## Mon activité entrepreneuriale
+
+C'est un concept connu en permaculture,
+Comme dans le sens commun :
+
+**Ne pas mettre tous ses oeufs dans le même panier.**
+
+J'en parlais dans [un récent article sur Medium](https://medium.com/@OhmWang/is-raw-better-2f857279cb2c),
+À force d'insister dans un domaine on en acquière l'expertise,
+et donc une meilleure rémunération.
+
+Mais dans cette expertise réside le danger
+De la disparition de la niche à laquelle elle se rattache.
+
+En focalisant mon activité entrepreneuriale sur
+Le développement web uniquement,
+Je prend un risque en même temps que j'accélère mon gain d'expertise.
+
+Mais parmi les modes de pensée,
+Il y a les spécialistes,
+Les généralistes et les pensées motif.
+
+Ce mode de fonctionnement (gain d'expertise => rémunération)
+n'est pas nécessairement adapté à tous les profims.
+
+Pour ma part, la pensée motif,
+et sa soif inextingible de nouvelles instances,
+Cherche le compromis entre les deux :
+
+**En poussant jusqu'à obtenir le motif.**
+
+### À propos de l'HIGHBS-BOK
+
+L'idée de proposer une histoire publiquement,
+sur internet,
+Et son site open-source,
+**est un business-model complet à inventer.**
+
+La problématique derrière est la rémunération de
+mon travail sur ce projet à la fois
+d'écriture, d'abstraction et de développement.
+
+En fait, pour m'inscrire et me fixer dans
+une activité que j'aime,
+Il faut que je puisse en vivre.
+
+La première piste est l'utilisation
+de plateformes telles que Tipeee ou Patreon.
+
+Déjà à ce stade se pose la
+nécessité de réaliser un travail communautaire important,
+régulier, intéressant
+**et qui donne envie de me sponsoriser**.
+
+La seconde piste est le vente de produits dérivés :
+
+* Version imprimée papier,
+* Impression d'*artworks* ...
+
+La troisième piste est l'abonnement pour
+l'accès aux commentaires écrits et vidéos,
+poser directement ses questions sur le site
+Et d'autres services à imaginer.
+
+### [Les storyboards](/storyboards), sur [imrok.fr](/)
+
+La question est encore plus délicate pour
+les storyboards que j'essaie d'archiver sur le site.
+
+Leur valeur, a priori, est bien moindre que
+l'HIGHBS-BOK dans sa version exposée/exposable.
+
+Toutefois, je pense qu'à titre d'exemple,
+Toutes les formes de progression dans un
+travail, une oeuvre, une entreprise
+a vertu à être exteriorisée et partagée.
+
+Ce contenu plus personnel
+exprime également mon état d'esprit
+au moment où je l'ai réalisé.
+
+De là à me faire rémunérer le temps,
+Pourtant très clair à établir,
+passé à scanner, retraiter et transcrire les
+centaines de pages concernées ...
+
+Qui cela pourrait-il intéresser ?
+`
+}

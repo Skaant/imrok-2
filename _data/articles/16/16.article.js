0828958ddc55c34cfa3074802bb16f01dd894548
imrok le bleu <romaric.ruga@gmail.com>
2021-01-12 08:14:23 +0100

    article 16

---
+++ _data/articles/16/16.article.js
@@ -0,0 +1,8 @@
+export default {
+  id: '16',
+  title: 'Les futurs articles de développement sur '
+    + 'mon site rimarok.com',
+  date: '10/01/2021',
+  tags: [ 'dev', 'rimarok.com' ],
+  description: false
+}
